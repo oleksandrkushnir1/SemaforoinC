96abab08377b9133bce6e337ddf58a617af4b806
Oleksandr-Kushnir <114574060+oleksandrkushnir1@users.noreply.github.com>
2023-11-03 09:42:12 +0000

    primocommit

---
+++ main.C
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int pulsante = 1;
+int i;
+
+void setSemV(pulsante){
+
+  if(pulsante == 0){
+  
+
+  sleep(1);
+  printf ("Il semaforo stradale: ⚪⚪🔴  \n"); 
+  
+  }
+    
+  else{
+      printf ("Il semaforo stradale: 🟢⚪⚪  \n");
+    }
+  
+}
+
+void setSemP(pulsante){
+
+  if(pulsante == 0){
+
+
+    printf ("Il semaforo pedonale : 🟢⚪  \n"); 
+    sleep(10);
+    
+      for (i = 0; i < 5; i++) {
+        printf(" Il semaforo pedonale:🟡🟡 \n");
+        sleep(1);
+        printf(" Il semaforo pedonale:⚪⚪ \n");
+        sleep(1);
+        
+      }
+   
+    
+   }
+  else {
+    printf(" Semaforo pedoni:⚪🔴 \n");
+  }
+}
+  
+  
+
+main (){
+  
+  for(;;){
+    setSemP(pulsante);
+    setSemV(pulsante);
+    printf ("Il pulsante è stato premuto? (true = 0): ");
+    scanf ("%d", &pulsante);
+    system("clear");
+    setSemV(pulsante);
+    setSemP(pulsante);
+    system("clear");
+    pulsante = 1;
+  }
+
+  
+}
